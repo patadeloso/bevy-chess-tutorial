ce7fbc91d6da93acbe969f81292a4cef79a28cef
Shaun McManus <shaun.mcmanus@gmail.com>
2021-09-24 22:52:26 -0700

    Pretty had to copy the repo since the tutorial was out of date

--- src/main.rs
+++ src/main.rs
@@ -1,233 +1,49 @@
-
 use bevy::prelude::*;
 use bevy_mod_picking::*;
 
 mod pieces;
 use pieces::*;
+mod board;
+use board::*;
+mod ui;
+use ui::*;
 
 fn main() {
     App::build()
-    // Set MSAA to 4 samples
-    .insert_resource(Msaa { samples: 4 })
-    // Set WindowDescriptor add_resource
-    .insert_resource(WindowDescriptor {
-        title: "Chess".to_string(),
-        width: 800.,
-        height: 800.,
-        ..Default::default()
-    })
-    .add_plugins(DefaultPlugins)
-    .add_plugin(PickingPlugin)
-    .add_plugin(InteractablePickingPlugin)
-    .add_plugin(HighlightablePickingPlugin)
-    .add_plugin(DebugCursorPickingPlugin)
-    .add_plugin(DebugEventsPickingPlugin)
-    .add_startup_system(setup.system())
-    .add_startup_system(create_board.system())
-    .add_startup_system(create_pieces.system())
-    .run();
+        // Set antialiasing to use 4 samples
+        .insert_resource(Msaa { samples: 4 })
+        // Set WindowDescriptor Resource to change title and size
+        .insert_resource(WindowDescriptor {
+            title: "Chess!".to_string(),
+            width: 600.,
+            height: 600.,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .init_resource::<PickingCamera>()
+        .add_plugin(PickingPlugin)
+        .add_plugin(BoardPlugin)
+        .add_plugin(PiecesPlugin)
+        .add_plugin(UIPlugin)
+        .add_startup_system(setup.system())
+        .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-
+fn setup(mut commands: Commands) {
+    commands
         // Camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
+        .spawn_bundle(PerspectiveCameraBundle {
             transform: Transform::from_matrix(Mat4::from_rotation_translation(
                 Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
                 Vec3::new(-7.0, 20.0, 4.0),
             )),
             ..Default::default()
-            })
-            .insert_bundle(PickingCameraBundle::default());
-
-
+        })
+        .insert_bundle(PickingCameraBundle::default())
         // Light
-    commands.spawn_bundle(LightBundle {
+        .commands()
+        .spawn_bundle(LightBundle {
             transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
             ..Default::default()
         });
-}
-
-fn create_board(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // Added meshes and materials
-    let mesh = meshes.add(Mesh::from(shape::Plane { size: 1. }));
-    let white_material = materials.add(Color::rgb(1., 0.9, 0.9).into());
-    let black_material = materials.add(Color::rgb(0., 0.1, 0.1).into());
-
-    // Spawn 64 squares
-    for i in 0..8 {
-        for j in 0..8 {
-            commands.spawn_bundle(PbrBundle {
-                mesh: mesh.clone(),
-                // Change material via position
-                material: if (i + j + 1) % 2 == 0 {
-                    white_material.clone()
-                } else {
-                    black_material.clone()
-                },
-                transform: Transform::from_translation(Vec3::new(i as f32, 0., j as f32)),
-                ..Default::default()
-            })
-            .insert_bundle(PickableBundle::default());
-        }
-    }
-}
-
-fn create_pieces(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // Load all the meshes
-    let king_handle: Handle<Mesh> =
-        asset_server.load("models/chess_kit/pieces.glb#Mesh0/Primitive0");
-    let king_cross_handle: Handle<Mesh> =
-        asset_server.load("models/chess_kit/pieces.glb#Mesh1/Primitive0");
-    let pawn_handle: Handle<Mesh> =
-        asset_server.load("models/chess_kit/pieces.glb#Mesh2/Primitive0");
-    let knight_1_handle: Handle<Mesh> =
-        asset_server.load("models/chess_kit/pieces.glb#Mesh3/Primitive0");
-    let knight_2_handle: Handle<Mesh> =
-        asset_server.load("models/chess_kit/pieces.glb#Mesh4/Primitive0");
-    let rook_handle: Handle<Mesh> =
-        asset_server.load("models/chess_kit/pieces.glb#Mesh5/Primitive0");
-    let bishop_handle: Handle<Mesh> =
-        asset_server.load("models/chess_kit/pieces.glb#Mesh6/Primitive0");
-    let queen_handle: Handle<Mesh> =
-        asset_server.load("models/chess_kit/pieces.glb#Mesh7/Primitive0");
-
-    // Add some materials
-
-    let white_material = materials.add(Color::rgb(1., 0.8, 0.8).into());
-    let black_material = materials.add(Color::rgb(0., 0.2, 0.2).into());
-
-    spawn_rook(
-        &mut commands,
-        white_material.clone(),
-        rook_handle.clone(),
-        Vec3::new(0., 0., 0.),
-    );
-    spawn_knight(
-        &mut commands,
-        white_material.clone(),
-        knight_1_handle.clone(),
-        knight_2_handle.clone(),
-        Vec3::new(0., 0., 1.),
-    );
-    spawn_bishop(
-        &mut commands,
-        white_material.clone(),
-        bishop_handle.clone(),
-        Vec3::new(0., 0., 2.),
-    );
-    spawn_queen(
-        &mut commands,
-        white_material.clone(),
-        queen_handle.clone(),
-        Vec3::new(0., 0., 3.),
-    );
-    spawn_king(
-        &mut commands,
-        white_material.clone(),
-        king_handle.clone(),
-        king_cross_handle.clone(),
-        Vec3::new(0., 0., 4.),
-    );
-    spawn_bishop(
-        &mut commands,
-        white_material.clone(),
-        bishop_handle.clone(),
-        Vec3::new(0., 0., 5.),
-    );
-    spawn_knight(
-        &mut commands,
-        white_material.clone(),
-        knight_1_handle.clone(),
-        knight_2_handle.clone(),
-        Vec3::new(0., 0., 6.),
-    );
-    spawn_rook(
-        &mut commands,
-        white_material.clone(),
-        rook_handle.clone(),
-        Vec3::new(0., 0., 7.),
-    );
-
-    for i in 0..8 {
-        spawn_pawn(
-            &mut commands,
-            white_material.clone(),
-            pawn_handle.clone(),
-            Vec3::new(1., 0., i as f32),
-        );
-    }
-
-    spawn_rook(
-        &mut commands,
-        black_material.clone(),
-        rook_handle.clone(),
-        Vec3::new(7., 0., 0.),
-    );
-    spawn_knight(
-        &mut commands,
-        black_material.clone(),
-        knight_1_handle.clone(),
-        knight_2_handle.clone(),
-        Vec3::new(7., 0., 1.),
-    );
-    spawn_bishop(
-        &mut commands,
-        black_material.clone(),
-        bishop_handle.clone(),
-        Vec3::new(7., 0., 2.),
-    );
-    spawn_queen(
-        &mut commands,
-        black_material.clone(),
-        queen_handle.clone(),
-        Vec3::new(7., 0., 3.),
-    );
-    spawn_king(
-        &mut commands,
-        black_material.clone(),
-        king_handle.clone(),
-        king_cross_handle.clone(),
-        Vec3::new(7., 0., 4.),
-    );
-    spawn_bishop(
-        &mut commands,
-        black_material.clone(),
-        bishop_handle.clone(),
-        Vec3::new(7., 0., 5.),
-    );
-    spawn_knight(
-        &mut commands,
-        black_material.clone(),
-        knight_1_handle.clone(),
-        knight_2_handle.clone(),
-        Vec3::new(7., 0., 6.),
-    );
-    spawn_rook(
-        &mut commands,
-        black_material.clone(),
-        rook_handle.clone(),
-        Vec3::new(7., 0., 7.),
-    );
-
-    for i in 0..8 {
-        spawn_pawn(
-            &mut commands,
-            black_material.clone(),
-            pawn_handle.clone(),
-            Vec3::new(6., 0., i as f32),
-        );
-    }
 }
